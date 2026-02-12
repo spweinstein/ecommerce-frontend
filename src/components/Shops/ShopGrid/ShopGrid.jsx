badce81463cc75647794360753a13b8104ec14c5
Spencer <sweinstein34@gmail.com>
2026-02-12 15:19:34 -0500

    Added shop industry filtering & product category filtering

--- src/components/Shops/ShopGrid/ShopGrid.jsx
+++ src/components/Shops/ShopGrid/ShopGrid.jsx
@@ -2,21 +2,41 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router";
 import * as shopService from "../../../services/shopService";
 import "./ShopGrid.css";
+import { getIndustries } from "../../../services/industryService";
 
 const ShopGrid = () => {
   const [shops, setShops] = useState([]);
+  const [industries, setIndustries] = useState([]);
+  const [selectedIndustry, setSelectedIndustry] = useState("");
 
   useEffect(() => {
     const fetchShops = async () => {
       try {
-        const shopData = await shopService.getShops();
+        const shopData = await shopService.getShops(selectedIndustry);
         setShops(shopData);
       } catch (err) {
         console.error(err);
       }
     };
     fetchShops();
-  }, []);
+  }, [selectedIndustry]);
+
+  useEffect(() => {
+    const fetchIndustries = async () => {
+      try {
+        const fetchedIndustries = await getIndustries();
+        setIndustries(fetchedIndustries);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchIndustries();
+  }, [shops]);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSelectedIndustry(value);
+  };
 
   return (
     <main className="shop-grid-container">
@@ -25,6 +45,23 @@ const ShopGrid = () => {
         <Link to="/shops/new" className="create-shop-btn">
           ➕ Create New Shop
         </Link>
+        <div>
+          <label htmlFor="industry">Filter Industry</label>
+          <select
+            name="industry"
+            id="industry"
+            required
+            onChange={handleChange}
+          >
+            <option value="">-- Select an option --</option>
+
+            {industries.map((industry) => (
+              <option key={industry._id} value={industry._id}>
+                {industry.name}
+              </option>
+            ))}
+          </select>
+        </div>
       </header>
 
       <div className="shop-grid">
